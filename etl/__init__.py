65bad28e53fd92a43b4703fe389fc69ff3aef7ae
Syver Petersen <syverpet@gmail.com>
2021-02-10 10:01:14 +0000

    # trans import in __init__ module

--- etl/__init__.py
+++ etl/__init__.py
@@ -3,6 +3,6 @@ from .ds import get_data, save_as_json_geojson, create_fname
 from .config import read_config
 from .db import DBController
 from .api_queries import query_rail, query_station, query_city
-from .trans import open_json
+#from .trans import open_json
 
 init_logger()
